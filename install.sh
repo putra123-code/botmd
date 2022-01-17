4ecd31a9e1906d24a313e7fa172274239e4a0f95
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-01-17 19:17:19 +0800

    botmd
    botmd

--- install.sh
+++ install.sh
@@ -6,7 +6,3 @@ pkg install wget
 pkg install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-cd node_modules/@adiwajshing/baileys-md
-npm i typescript -g
-tsc
-cd ../../../
